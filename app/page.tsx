ec88e497810be6741c1bb457c56a48d1821435f0
Matthew Bennett <mattsbennett@gmail.com>
2024-01-13 18:23:10 -0800

    Add privacy policy

--- app/page.tsx
+++ app/page.tsx
@@ -4,32 +4,34 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full font-mono items-center justify-between text-sm lg:flex">
-      <div className="fixed bottom-0 left-0 flex h-48 w-full items-center justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-center justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <Image
             src="/lampflame.svg"
             alt="Lamp Logo"
-            width={20}
-            height={40}
+            width={25}
+            height={50}
             priority
           />
-          <span className="pl-2 pt-2">Lamp Bible</span>
+          <span className="pl-2 pt-2 text-lg">Lamp Bible</span>
         </div>
-        <p className="fixed left-0 top-0 text-xs flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="fixed left-0 top-0 text-s flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Bible Companion App
         </p>
       </div>
 
       <div className="relative flex place-items-center">
-        <Image
-          src="/lampicon.png"
-          alt="Lamp App Icon"
-          width={200}
-          height={200}
-          priority
-        />
+        <a href="#" target="_blank">
+          <Image
+            src="/lampicon.png"
+            alt="Lamp App Icon"
+            width={200}
+            height={200}
+            priority
+          />
+        </a>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-2 lg:text-left">
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-center">
         <a
           href="mailto:%6c%61%6d%70%62%69%62%6c%65%61%70%70%40%67%6d%61%69%6c%2e%63%6f%6d"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -46,15 +48,40 @@ export default function Home() {
             Contact the developer with support or feature requests.
           </p>
         </a>
-        <div className="relative flex place-items-center justify-center lg:justify-end">
+        <a
+          href="/privacy"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Privacy Policy{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 text-sm opacity-50`}>
+            Read Lamp Bible&apos;s privacy policy.
+          </p>
+        </a>
+        <a
+          href="#"
+          className="flex flex-wrap justify-center group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold w-full`}>
+            Download{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
           <Image
             src="/appstore.svg"
             alt="Lamp App Icon"
-            width={150}
-            height={50}
+            className='mt-1'
+            width={140}
+            height={40}
             priority
           />
-        </div>
+        </a>
       </div>
     </main>
   )
---
+++ app/privacy/page.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Privacy() {
+    return (
+      <main className="flex min-h-screen flex-col justify-between p-24">
+        <div className="z-10 max-w-5xl w-full font-mono items-center justify-between text-sm lg:flex">
+        <Link href="/">
+            <div className="fixed bottom-0 left-0 flex h-48 w-full items-center justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+                <Image
+                    src="/lampflame.svg"
+                    alt="Lamp Logo"
+                    width={20}
+                    height={40}
+                    priority
+                />
+                <span className="pl-2 pt-2">Lamp Bible</span>
+            </div>
+        </Link>
+        </div>
+        <div className='flex min-h-screen flex-col justify-between pt-16'>
+            <h2 className="text-3xl font-bold">Privacy Policy</h2>
+            <h3 className="text-2xl">Information Collection and Use</h3>
+            <p className='leading-loose'>Lamp Bible does not collect any personal information or user data. We do not gather, use, or share any information about your device, location, or activities within the app.</p>
+            <h3 className="text-2xl">User Tracking</h3>
+            <p className='leading-loose'>We do not employ any user tracking or analytics tools within Lamp Bible. Your usage of the app is not monitored, and no data is collected for the purpose of tracking your behavior.</p>
+            <h3 className="text-2xl">Third-Party Services</h3>
+            <p className='leading-loose'>Lamp Bible does not integrate with any third-party services that collect user data. We do not share any information with external parties.</p>
+            <h3 className="text-2xl">Contact Us</h3>
+            <p className='leading-loose'>If you have any questions about our Privacy Policy, please contact us at lampbibleapp[at]gmail[dot]com.</p>
+            <h3 className="text-2xl">Changes to This Privacy Policy</h3>
+            <p className='leading-loose'>Lamp Bible reserves the right to make changes to this Privacy Policy. Any updates will be communicated within the app or on our website.</p>
+            <p className='leading-loose'>By using Lamp Bible, you agree to the terms of this Privacy Policy. If you do not agree with these terms, please do not use the app.</p>
+            <p className='leading-loose'>This Privacy Policy was last updated on Jan. 13, 2024.</p>
+        </div>
+      </main>
+    )
+  }
