d552bcdf5efb99040a706e821da5166b182c5a50
Matthew Bennett <mattsbennett@gmail.com>
2024-01-14 16:55:14 -0800

    Change contact link href to github issues

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-center">
         <a
-          href="mailto:%6c%61%6d%70%62%69%62%6c%65%61%70%70%40%67%6d%61%69%6c%2e%63%6f%6d"
+          href="https://github.com/mattsbennett/lamp-bible-ios/issues"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
